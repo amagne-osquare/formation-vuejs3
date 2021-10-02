de1e46b03f0df5c929e50fd81f34650500d869fd
Marion <marion@les-tilleuls.coop>
2021-10-02 17:24:47 +0200

    add homepage view

---
+++ vue-app/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="section">
+        <h1 class="title has-text-primary">{{ title }}</h1>
+
+        <figure class="image is-3by1">
+            <img :src="imageUrl" />
+        </figure>
+
+        <hr />
+
+        <!-- insert featured products -->
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Bienvenue !',
+            imageUrl: 'https://picsum.photos/id/191/2246/749'
+        };
+    },
+}
+</script>
