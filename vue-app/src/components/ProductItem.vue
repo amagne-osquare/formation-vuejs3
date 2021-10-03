e6b6f4f5548d0ae91d293d4e4a24ff8d26f26ce3
Marion Agé <marion@les-tilleuls.coop>
2021-10-03 15:41:20 +0200

    add product item component

---
+++ vue-app/src/components/ProductItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="card product">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <router-link :to="product['@id']">
+            <img :src="product.imageUrl" :alt="product.name">
+          </router-link>
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">
+              <router-link :to="product['@id']">{{ product.name }}</router-link>
+            </p>
+
+            <div class="tags are-large is-pulled-right">
+              <span class="tag is-primary is-rounded">
+                {{ format(product.price) }}
+              </span>
+              <span v-if="product.inSale" class="tag is-warning is-rounded">PROMO</span>
+            </div>
+          </div>
+        </div>
+        <div class="is-flex is-justify-content-space-between is-align-items-baseline">
+          <div class="field is-grouped is-grouped-multiline">
+            <div
+              v-for="variant in product.variants"
+              :key="variant['@id']"
+              class="control"
+            >
+              <div class="tags has-addons">
+                <span class="tag" :style="{ backgroundColor: variant.value }"></span>
+                <span class="tag is-white">{{ variant.value }}</span>
+              </div>
+            </div>
+          </div>
+          <button class="button is-success" :disabled="0 === product.variants.length">Voir le produit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: { type: Object, required: true },
+  },
+  methods: {
+    format(price) {
+      return new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price / 100);
+    },
+  }
+}
+</script>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -20,56 +20,12 @@
       </div>
 
       <div class="columns products is-multiline">
-        <div
+        <ProductItem
           v-for="product in filteredProducts"
           :key="product['@id']"
+          :product="product"
           class="column is-one-quarter"
-        >
-
-          <div class="card product">
-            <div class="card-image">
-              <figure class="image is-4by3">
-                <a href="#">
-                  <img :src="product.imageUrl" :alt="product.name">
-                </a>
-              </figure>
-            </div>
-            <div class="card-content">
-              <div class="media">
-                <div class="media-content">
-                  <div class="is-flex is-justify-content-space-between is-align-items-baseline">
-                    <p class="title is-4">
-                      <a href="#">{{ product.name }}</a>
-                    </p>
-
-                    <div class="tags are-large is-pulled-right">
-                      <span class="tag is-primary is-rounded">
-                        {{ format(product.price) }}
-                      </span>
-                      <span v-if="product.inSale" class="tag is-warning is-rounded">PROMO</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="is-flex is-justify-content-space-between is-align-items-baseline">
-                <div class="field is-grouped is-grouped-multiline">
-                  <div
-                    v-for="variant in product.variants"
-                    :key="variant['@id']"
-                    class="control"
-                  >
-                    <div class="tags has-addons">
-                      <span class="tag" :style="{ backgroundColor: variant.value }"></span>
-                      <span class="tag is-white">{{ variant.value }}</span>
-                    </div>
-                  </div>
-                </div>
-                <button class="button is-success" :disabled="0 === product.variants.length">Voir le produit</button>
-              </div>
-            </div>
-          </div>
-
-        </div>
+        />
       </div>
     </template>
   </section>
@@ -77,8 +33,12 @@
 
 <script>
 import { axiosInstance } from '@/api/axios';
+import ProductItem from '@/components/ProductItem.vue';
 
 export default {
+  components: {
+    ProductItem,
+  },
   data() {
     return {
       featured: [],
@@ -100,11 +60,6 @@ export default {
       return this.featured.filter((product) => product.inSale);
     }
   },
-  methods: {
-    format(price) {
-      return new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price / 100);
-    },
-  }
 }
 </script>
 
